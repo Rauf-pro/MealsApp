a469964dbe29383a99d8efc01077b39992039268
rrauf8178 <rrauf8178@gmail.com>
2022-11-11 18:58:34 +0400

    adding and coniguring the mealdetail screen

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealDetailScreen from "./screens/MealDetailScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="dark" />
+      <StatusBar style="light" />
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{
@@ -26,7 +27,17 @@ export default function App() {
               title: "All Categories",
             }}
           />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            // options={({route, navigation}) => {            // Dinamically options
+            //   const catId = route.params.categoryId;
+            //   return {
+            //       title: catId,
+            //   };
+            // }}                  
+          />
+          <Stack.Screen name="MealDetail" component={MealDetailScreen}/>
         </Stack.Navigator>
       </NavigationContainer>
     </>
