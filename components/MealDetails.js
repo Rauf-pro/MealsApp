159d8408474b7fdb8e46ace4dd6f305b16623587
rrauf8178 <rrauf8178@gmail.com>
2022-11-13 01:13:25 +0400

    finishing the meal detail screen

--- components/MealDetails.js
+++ components/MealDetails.js
@@ -1,11 +1,21 @@
 import { Text, View, StyleSheet } from "react-native";
 
-function MealDetails({duration, complexity, affordability}) {
+function MealDetails({
+  duration,
+  complexity,
+  affordability,
+  style,
+  textStyle,
+}) {
   return (
-    <View style={styles.details}>
-      <Text style={styles.detailItem}>{duration}m</Text>
-      <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-      <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+    <View style={[styles.details, style]}>
+      <Text style={[styles.detailItem, textStyle]}>{duration}m</Text>
+      <Text style={[styles.detailItem, textStyle]}>
+        {complexity.toUpperCase()}
+      </Text>
+      <Text style={[styles.detailItem, textStyle]}>
+        {affordability.toUpperCase()}
+      </Text>
     </View>
   );
 }
@@ -13,14 +23,14 @@ function MealDetails({duration, complexity, affordability}) {
 export default MealDetails;
 
 const styles = StyleSheet.create({
-    details: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 8,
-      },
-      detailItem: {
-        marginHorizontal: 4,
-        fontSize: 12,
-      },
-})
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+});
