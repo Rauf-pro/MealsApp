f3cfa7ea6f22e6a03b4baee20a93e6928da77d74
rrauf8178 <rrauf8178@gmail.com>
2022-11-14 00:39:41 +0400

    Working with Redux Slices

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import "react-native-gesture-handler";
 import FavoritesScreen from "./screens/FavoriteScreen";
 import { Ionicons } from "@expo/vector-icons";
 import FavoritesContextProvider from "./store/context/favorites-context";
+import { Provider } from "react-redux";
+import { store } from "./store/redux/store";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -53,7 +55,8 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
@@ -80,7 +83,8 @@ export default function App() {
             />
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+      </Provider>
+      {/* </FavoritesContextProvider> */}
     </>
   );
 }
